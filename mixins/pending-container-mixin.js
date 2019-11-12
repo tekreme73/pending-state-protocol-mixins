7a2930d4ec70ca8acb514febcec121f675bd4d6b
Rémi Rebillard <tekreme73@gmail.com>
2019-11-12 18:47:32 +0100

    Add pending-container-mixin.js extracted from Justin talk https://youtu.be/x9YDQUJx2uw?t=1526

---
+++ mixins/pending-container-mixin.js
@@ -0,0 +1,17 @@
+export const PendingContainer = (base) => class extends base {
+
+    constructor() {
+        super();
+
+        this.__hasPendingChildren = false;
+        this.__pendingCount = 0;
+
+        this.addEventListener('pending-state', async e => {
+            this.__hasPendingChildren = true;
+            this.__pendingCount++;
+            await e.detail.promise;
+            this.__pendingCount--;
+            this.__hasPendingChildren = this.__pendingCount !== 0;
+        });
+    }
+};
